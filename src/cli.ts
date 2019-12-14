9fbb0676f8112af563b625d601f5fb711351996d
Renan Machado <renan.mav@hotmail.com>
2019-12-14 10:43:05 -0300

    feat(multiple-files): add regex flag

--- src/cli.ts
+++ src/cli.ts
@@ -7,9 +7,13 @@ export const run = () => {
   const argv = yargs(process.argv)
     .usage('Sort an object’s key within source code automatically as part of linting')
     .describe('src', 'The source to sort object’s keys')
-    .default('src', process.cwd()).argv
+    .default('src', process.cwd())
+    .describe('regex', 'Regex to match files')
+    .default('regex', /./g).argv
 
   const jsFiles = shell.find(argv.src).filter(path => /\.(js|ts|tsx)$/.test(path))
 
-  generateResources(jsFiles)
+  const regex = new RegExp(argv.regex)
+
+  generateResources(jsFiles.filter(file => regex.test(file)))
 }
