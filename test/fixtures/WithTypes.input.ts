ee8b20906042f3c1b7cd953b205b6a214ccb0a6a
Renan Machado <renan.mav@hotmail.com>
2019-12-14 10:02:03 -0300

    feat(multiple-files): add second fixture

---
+++ test/fixtures/WithTypes.input.ts
@@ -0,0 +1,10 @@
+const coolThings = {
+  Star: 'Wars',
+  TheBigBang: 'Theory',
+  The: 'Office',
+  Breaking: 'Bad',
+}
+
+export default coolThings
+
+export type Keys = keyof typeof coolThings
---
+++ test/fixtures/WithTypes.output.ts
@@ -0,0 +1,10 @@
+const coolThings = {
+  Breaking: 'Bad',
+  The: 'Office',
+  TheBigBang: 'Theory',
+  Star: 'Wars',
+}
+
+export default coolThings
+
+export type Keys = keyof typeof coolThings
