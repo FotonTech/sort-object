9c33eaec8a54326c0aeb7f21422658f6e3316124
Raphael Thomazella <thomazella9@gmail.com>
2020-01-20 20:39:29 -0300

    chore(misc): update config

--- dangerfile.js
+++ dangerfile.js
@@ -2,3 +2,7 @@ import { message, danger } from 'danger';
 
 const modifiedMD = danger.git.modified_files.join('- ');
 message('Changed Files in this PR: \n - ' + modifiedMD);
+
+const len = danger.git.commits.length
+message('commits: \n - ' + len);
+message('Changed Files in this PR: \n - ' + modifiedMD);
